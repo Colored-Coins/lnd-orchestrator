ed1e9032c314063c613f4b26d49db68ed505e5e8
Nadav Ivgi <nadav@shesek.info>
2016-11-01 22:04:23 +0200

    remove auto block mining from client-init, done in a separate deamon

--- scripts/client-init.sh
+++ scripts/client-init.sh
@@ -7,13 +7,5 @@ hubpeerid=`lncli --rpcserver $HUB_RPC connect $1@$LND_HOST:$2 | jq -r '.peer_id'
 
 echo "hub connected (peer #$hubpeerid), opening channel..."
 
-lncli --rpcserver $HUB_RPC openchannel --peer_id $hubpeerid --local_amt $HUB_FUNDING_AMT --remote_amt 0 --num_confs 1 &
-echo openchannel req: lncli --rpcserver $HUB_RPC openchannel --peer_id $hubpeerid --local_amt $HUB_FUNDING_AMT --remote_amt 0 --num_confs 1
-
-echo waiting for open tx to finalize and be sent...
-
-sed '/PEER: Creating chan barrier for ChannelPoint /q' <(tail -n 1 -f $LND_PATH/$3/proc.log)
-echo "tx ready!"
-echo  btcctl --simnet --wallet --rpcserver $BTCW_HOST --rpcuser "$BTCW_USER" --rpcpass "$BTCW_PASS" generate 1
-btcctl --simnet --wallet --rpcserver $BTCW_HOST --rpcuser "$BTCW_USER" --rpcpass "$BTCW_PASS" generate 1
+lncli --rpcserver $HUB_RPC openchannel --peer_id $hubpeerid --local_amt $HUB_FUNDING_AMT --remote_amt 0 --num_confs 1
 
