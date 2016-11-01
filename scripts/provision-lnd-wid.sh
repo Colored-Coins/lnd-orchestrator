725f9e48c5bf3d2c03e6649ee03f958883e2bcb2
Nadav Ivgi <nadav@shesek.info>
2016-11-01 17:17:42 +0200

    write stderr to provision.log

--- scripts/provision-lnd-wid.sh
+++ scripts/provision-lnd-wid.sh
@@ -5,6 +5,6 @@ set -o pipefail
 
 # Run provision-lnd.sh, wait until the __WID__ line appears, then print the
 # wid and exit immediately (while keeping provision-lnd's jobs running in the background)
-sed '/^__WID__ / q' <(./provision-lnd.sh | tee -a /tmp/provision.log &) | tail -n 1
+sed '/^__WID__ / q' <(./provision-lnd.sh 2>&1 | tee -a /tmp/provision.log &) | tail -n 1
 
 #disown
